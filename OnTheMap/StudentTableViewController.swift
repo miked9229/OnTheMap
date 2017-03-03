33c1e7d596eadb45626b2cd953d569d9ea0b5e3a
Michael Doroff <michaeldoroff@Michaels-MacBook-Air.local>
2017-03-02 21:08:02 -0600

    Add StudentTableViewController and View

---
+++ OnTheMap/StudentTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  StudentTableViewController.swift
+//  OnTheMap
+//
+//  Created by Michael Doroff on 3/2/17.
+//
+//
+
+import Foundation
+import UIKit
+
+
+class StudentTableViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+}
