759b4403c421c385cd9b4aef71af0626334b22a6
Michael Doroff <michaeldoroff@Michaels-MacBook-Air.local>
2017-03-12 11:20:12 -0500

    Add StudentInformationPostingViewController

---
+++ OnTheMap/StudentInformationPostingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  StudentInformationPostingViewController.swift
+//  OnTheMap
+//
+//  Created by Michael Doroff on 3/12/17.
+//
+//
+
+import Foundation
+import UIKit
+
+class StudentInformationPostingViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+    
+}
