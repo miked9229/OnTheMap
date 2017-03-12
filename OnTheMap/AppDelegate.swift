bcf12f79e9ce96ea813d2338b1228083a6b1ada5
Michael Doroff <michaeldoroff@Michaels-MacBook-Air.local>
2017-03-12 15:09:09 -0500

    Add String to GeoCode Converter to StudentInformationPostingViewController

--- OnTheMap/AppDelegate.swift
+++ OnTheMap/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     var studentLocations = [StudentLocation]()
+    var userLocation: String = ""
     
 
 
--- OnTheMap/StudentInformationPostingViewController.swift
+++ OnTheMap/StudentInformationPostingViewController.swift
@@ -8,14 +8,62 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 class StudentInformationPostingViewController: UIViewController {
     
     
     
+    @IBOutlet weak var locationTextField: UITextField!
     
     
+    @IBAction func Cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     
     
+    }
+    
+    
+    @IBAction func FindOnMap(_ sender: Any) {
+        let annotation = MKPointAnnotation()
+        
+        
+        guard let addressFromString = locationTextField.text else {
+            raiseAddressError()
+            return
+        }
+        
+        let address = addressFromString
+        let geocoder = CLGeocoder()
+    
+        geocoder.geocodeAddressString(address) {(result, error) in
+            
+            guard let result = result else {
+                self.raiseAddressError()
+                return
+            }
+            
+            if (error != nil) {
+                self.raiseAddressError()
+                return
+            }
+            
+            
+            let latitude = (result[0].location?.coordinate.latitude)
+            let longitude = (result[0].location?.coordinate.longitude)
+            let coordinate = CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!)
+           
+            
+            print(coordinate)
+      
+            annotation.coordinate = coordinate
+            
+        }
+    
+        
+    }
+    func raiseAddressError() {
+        print("There was an address error")
+    }
     
 }
