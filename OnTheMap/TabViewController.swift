9ca0f80caaa55a7227a1c5ff9b9cf1f596ff9b5f
Michael Doroff <michaeldoroff@Michaels-MacBook-Air.local>
2017-03-18 19:36:02 -0500

    Finished ReloadData Method for TabBarViewController

--- OnTheMap/TabViewController.swift
+++ OnTheMap/TabViewController.swift
@@ -30,22 +30,29 @@ class TabViewController: UITabBarController {
     }
     
     @IBAction func reloadData(_ sender: Any) {
-       print("Method called")
-        
+ 
         self.view.alpha = 0.25
         activityIndicator.center = self.view.center
         activityIndicator.activityIndicatorViewStyle = .whiteLarge
         activityIndicator.color = UIColor.blue
         view.addSubview(activityIndicator)
-        
         activityIndicator.startAnimating()
         
         ParseClient.sharedInstance().getUserData() {(data, error) in
+
+            
             if error == nil {
                 if let data = data {
                     let appDelegate = UIApplication.shared.delegate as! AppDelegate
                     appDelegate.studentLocations = []
                     appDelegate.studentLocations = data
+                  
+                    
+                    performUIUpdatesOnMain {
+                        self.activityIndicator.stopAnimating()
+                        self.view.alpha = 1.0
+                        self.view.reloadInputViews()
+                    }
                     
                     
                     
